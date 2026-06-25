d3ee3ffe21f5033c4c697c6ca37a72b6dbbdd092
魏无羡 <wikelman33@protonmail.com>
2026-06-25 04:35:29 -0400

    Codigos finales corregidos

--- app.js
+++ app.js
@@ -5,16 +5,15 @@ const SUPABASE_ANON_KEY = 'sb_publishable_TKO64m749jBBQCAbh4SVbA_aC0hplzx'
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 
 async function listarProductos() {
-    // Pedimos todo (*) de la tabla productos
     const { data, error } = await supabase.from('productos').select('*')
     
     if (error) {
-        console.error("Error al obtener datos:", error)
+        console.error("Error:", error.message)
         return
     }
     
     const tabla = document.getElementById('tabla-productos')
-    if (data && data.length > 0) {
+    if (data) {
         tabla.innerHTML = data.map(p => `
             <tr>
                 <td>${p.nombre}</td>
@@ -23,8 +22,6 @@ async function listarProductos() {
                 <td>${p.stock}</td>
             </tr>
         `).join('')
-    } else {
-        tabla.innerHTML = "<tr><td colspan='4'>No hay datos disponibles</td></tr>"
     }
 }
 
